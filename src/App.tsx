b14f28d84e3da6bc9b79a6d2e04dfe0ed8399a90
noko-git <kotaroyamano0725@gmail.com>
2025-01-02 23:24:14 +0900

    add sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,21 @@ import UploadPage from './pages/UploadPage';
 import SearchResultPage from './pages/SearchResultPage';
 import HistoryListPage from './pages/HistoryListPage';
 import HistoryDetailPage from './pages/HistoryDetailPage';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-gray-100">
-        <Routes>
-          <Route path="/" element={<UploadPage />} />
-          <Route path="/search-result" element={<SearchResultPage />} />
-          <Route path="/history" element={<HistoryListPage />} />
-          <Route path="/history/:id" element={<HistoryDetailPage />} />
-        </Routes>
+      <div className="flex">
+        <Sidebar />
+        <div className="min-h-screen bg-gray-100 flex-grow">
+          <Routes>
+            <Route path="/" element={<UploadPage />} />
+            <Route path="/search-result" element={<SearchResultPage />} />
+            <Route path="/history" element={<HistoryListPage />} />
+            <Route path="/history/:id" element={<HistoryDetailPage />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );
